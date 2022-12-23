0075fa0040d6388391e57eb297e12c8416cbcdd2
Hanna <anya.mshar@gmail.com>
2022-12-23 17:34:53 +0100

    added conftest.py and test_items.py

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_should_find_button_to_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert button is not None, 'Нельзя добавить в корзину! Ошибка!'
